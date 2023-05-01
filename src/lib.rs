d0564ec856b4177f7fece0a1cb597e4e7a7e8058
Gustavo Michels de Camargo <guga.power@hotmail.com>
2023-05-01 19:08:10 -0300

    Tentei adicionar testes unitarios mas não deu certo

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,7 @@
 use pyo3::prelude::*;
 use std::sync::{Arc, Mutex, MutexGuard};
 
-// 1. Mudar de "Arc<Mutex<Node>>" para Option<Arc<Mutex<Node>>> para permitir listas vazias
-// 2. Uma função para contar o numero de itens presentes na lista circular
-// 3. Adicionar um limite de itens na lista circular, e que ao tentar adicionar um novo item, caso o limite seja atingido, o item mais antigo seja removido
-// 4. Uma função para retunar todos os itens da lista circular como um vetor (que no Python é uma lista)
-// 5. Adicioar as tiagens
-
+// Definindo a estrutura Circle e Node
 #[pyclass]
 pub struct Circle {
     head: Option<Arc<Mutex<Node>>>,
@@ -19,8 +14,10 @@ struct Node {
     last: Option<Arc<Mutex<Node>>>,
 }
 
+// Implementando métodos para a estrutura Circle
 #[pymethods]
 impl Circle {
+    // Inicializa um objeto Circle com tamanho máximo opcional
     #[new]
     pub fn new(max_size: Option<usize>) -> Circle {
         Circle {
@@ -29,6 +26,7 @@ impl Circle {
         }
     }
 
+    // Retorna o valor do nó atual (cabeça) da lista circular
     pub fn current_value(&self) -> PyObject {
         match self.head {
             Some(ref x) => {
@@ -39,6 +37,8 @@ impl Circle {
             None => Python::with_gil(|py| py.None()),
         }
     }
+
+    // Remove o nó atual e retorna seu valor
     pub fn remove_current(&mut self) -> PyObject {
         let ret: PyObject = self.current_value();
         if let Some(ref head) = self.head {
@@ -63,87 +63,83 @@ impl Circle {
         return ret;
     }
 
+    // Movem a cabeça da lista para o próximo nó e retornam o valor do nó atualizado
     pub fn move_next(&mut self) -> PyObject {
-        if let Some(ref head) = self.head {
-            let next = {
-                let head_guard: MutexGuard<Node> = head.lock().unwrap();
-                head_guard.next.as_ref().map(Clone::clone)
-            };
-
-            if let Some(next) = next {
-                self.head = Some(next);
-            }
-        }
-
-        return self.current_value();
+        self.move_to(true)
     }
 
+    // Movem a cabeça da lista para o nó anterior e retornam o valor do nó atualizado
     pub fn move_previous(&mut self) -> PyObject {
+        self.move_to(false)
+    }
+
+    fn move_to(&mut self, move_to_next: bool) -> PyObject {
         if let Some(ref head) = self.head {
-            let last = {
+            let adjacent_node = {
                 let head_guard: MutexGuard<Node> = head.lock().unwrap();
-                head_guard.last.as_ref().map(Clone::clone)
+                if move_to_next {
+                    head_guard.next.as_ref().map(Clone::clone)
+                } else {
+                    head_guard.last.as_ref().map(Clone::clone)
+                }
             };
 
-            if let Some(last) = last {
-                self.head = Some(last);
+            if let Some(adjacent_node) = adjacent_node {
+                self.head = Some(adjacent_node);
             }
         }
 
         return self.current_value();
     }
 
+    // Insere um novo nó com valor fornecido após o nó atual
+    // Se o tamanho máximo for atingido, o nó mais antigo é removido
     pub fn insert_after_current(&mut self, val: PyObject) {
+        self.insert_node(val, true);
+    }
+
+    // Insere um novo nó com valor fornecido antes do nó atual
+    // Se o tamanho máximo for atingido, o nó mais antigo é removido
+    pub fn insert_before_current(&mut self, val: PyObject) {
+        self.insert_node(val, false);
+    }
+
+    fn insert_node(&mut self, val: PyObject, insert_after: bool) {
         if let Some(max_size) = self.max_size {
             if self.count() >= max_size {
-                self.move_next();
+                if insert_after {
+                    self.move_next();
+                } else {
+                    self.move_previous();
+                }
                 self.remove_current();
-                self.move_previous();
-
-                self.max_size = Some(max_size - 1);
             }
         }
 
         if let Some(ref head) = self.head {
-            let (node, mut head) = {
+            let (adjacent_node, mut head) = {
                 let head_guard: MutexGuard<Node> = head.lock().unwrap();
-                (head_guard.next.as_ref().map(Clone::clone), head.clone())
+                if insert_after {
+                    (head_guard.next.as_ref().map(Clone::clone), head.clone())
+                } else {
+                    (head_guard.last.as_ref().map(Clone::clone), head.clone())
+                }
             };
             let mut new_node: Arc<Mutex<Node>> = Node::new(val);
             Node::combine(&mut head, &mut new_node);
-            Node::combine(&mut new_node, &mut node.unwrap_or(head));
+            Node::combine(&mut new_node, &mut adjacent_node.unwrap_or(head));
         } else {
             let new_node = Node::new(val);
             self.head = Some(new_node);
         }
     }
 
-    pub fn insert_before_current(&mut self, val: PyObject) {
-        if let Some(max_size) = self.max_size {
-            if self.count() >= max_size {
-                self.move_previous();
-                self.remove_current();
-            }
-        }
-
-        if let Some(ref head) = self.head {
-            let (node, mut head) = {
-                let head_guard = head.lock().unwrap();
-                (head_guard.last.as_ref().map(Clone::clone), head.clone())
-            };
-            let mut new_node: Arc<Mutex<Node>> = Node::new(val);
-            Node::combine(&mut new_node, &mut head);
-            Node::combine(&mut node.unwrap_or(head), &mut new_node);
-        } else {
-            let new_node = Node::new(val);
-            self.head = Some(new_node);
-        }
-    }
-
+    // Verifica se a lista está vazia
     pub fn is_empty(&self) -> bool {
         return self.head.is_none();
     }
 
+    // Conta o número de itens na lista circular
     pub fn count(&self) -> usize {
         if self.is_empty() {
             return 0;
@@ -169,6 +165,7 @@ impl Circle {
         return count;
     }
 
+    // Retorna todos os itens da lista circular como um vetor (lista em Python)
     pub fn to_vec(&self) -> Vec<PyObject> {
         let mut ret: Vec<PyObject> = Vec::new();
 
@@ -236,4 +233,33 @@ pub fn rust_lista_circular(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<Circle>()?;
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    // use pyo3::types::PyString;
+
+    #[test]
+    fn test_circle_empty() {
+        //     Python::with_gil(|_py| {
+        //         let circle = Circle::new(None);
+        //         assert!(circle.is_empty());
+        //     });
+        let circle = Circle::new(None);
+        assert!(circle.is_empty());
+    }
+
+    // #[test]
+    // fn test_circle_insert_and_count() {
+    //     let gil = Python::acquire_gil();
+    //     let py = gil.python();
+    //     let mut circle = Circle::new(None);
+
+    //     circle.insert_after_current(py.None());
+    //     assert_eq!(circle.count(), 1);
+
+    //     circle.insert_after_current(py.None());
+    //     assert_eq!(circle.count(), 2);
+    // }
 }
